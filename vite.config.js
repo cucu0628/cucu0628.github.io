aa8af1c6c778d1d2bfb3295e8e91125116348aca
cucu0628 <adambence2007@gmail.com>
2025-10-20 18:48:12 +0200

    asd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'cucu0628.github.io',
+  base: '/',
   plugins: [react(),tailwindcss(),],
 })
